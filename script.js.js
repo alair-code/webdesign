997acb6031cc1bdd83e1fcdee265a2d3a67e0d4b
Alair Soares <alairapolinariosoares@gmail.com>
2026-07-26 18:23:28 -0300

    Exibe um alerta quando o botão é clicado

---
+++ script.js.js
@@ -0,0 +1,5 @@
+const botao = document.getElementById('botao-mensagem'); // Seleciona o botão pelo ID   
+
+botao.addEventListener('click', function() { // Adiciona um evento de clique ao botão
+    alert('Mensagem enviada com sucesso!'); // Exibe um alerta quando o botão é clicado
+});

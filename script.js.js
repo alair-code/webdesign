3ad64e82c5eec75e97866ece70744a9b97bdaec2
Alair Soares <alairapolinariosoares@gmail.com>
2026-07-26 19:13:28 -0300

    Exibe outro alerta de agradecimento });

--- script.js.js
+++ script.js.js
@@ -2,4 +2,6 @@ const botao = document.getElementById('botao-mensagem'); // Seleciona o botão p
 
 botao.addEventListener('click', function() { // Adiciona um evento de clique ao botão
     alert('Mensagem enviada com sucesso!'); // Exibe um alerta quando o botão é clicado
+    alert('Obrigado por entrar em contato!'); // Exibe outro alerta de agradecimento
 });
+

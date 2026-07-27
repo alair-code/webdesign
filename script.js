2895c59cfd0acb8844809abce00129e9fe099543
Alair Soares <alairapolinariosoares@gmail.com>
2026-07-27 13:02:48 -0300

    Criar	um	novo parágrafo	e	adicioná-lo	a	uma	seção	existente

--- script.js
+++ script.js
@@ -9,5 +9,10 @@ botao.addEventListener('click', function() { // Adiciona um evento de clique ao 
 const titulo = document.getElementById('titulo'); // Seleciona o título pelo ID
 titulo.textContent = 'Novo Título'; // Atualiza o texto do título   
 
-//localizar	o	elemento	pelo	seu	id	e	atualizar	seu conteúdo	usando	o	método	inerText
-document.getElementById('titulo').inerText='Texto atualizado com JavaScript';
+//localizar	o	elemento	pelo	seu	id	e	atualizar	seu conteúdo	usando	o	método	innerText
+document.getElementById('titulo').innerText='Texto atualizado com JavaScript';
+
+//Criar	um	novo parágrafo	e	adicioná-lo	a	uma	seção	existente:
+const novaDiv = document.creatElement('p')//cria novo parágrafo
+novaDiv.innerText ='Este parágragrafo foi criado dinamicamente!';
+document.getElementById('conteudo').appendChild(novaDiv); //Adiciona o parágrafo a div existente
